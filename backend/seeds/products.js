af2ba3255eefa8b3917ec5af264a3c425efb6a61
piyushxyz1 <piyushnegi033@gmail.com>
2025-11-28 11:14:24 +0530

    changes made

--- backend/seeds/products.js
+++ backend/seeds/products.js
@@ -5,49 +5,37 @@ const SAMPLE_PRODUCTS = [
     name: "Vitamin B12",
     price: 299.99,
     image:
-      "https://images.pexels.com/photos/29278040/pexels-photo-29278040.jpeg",
+      "https://images.unsplash.com/photo-1690959794962-d8b3a18aa784?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dml0YW1pbiUyMGIxMnxlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     name: "Zinc Citrate",
     price: 159.99,
     image:
-      "https://cdn.nutrabay.com/wp-content/uploads/2021/11/NB-OPT-1012-04-01.jpg",
+      "https://images.unsplash.com/photo-1695634237714-81815523807b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTIzfHx6aW5jJTIwY2l0cmF0ZSUyMGNvbnRhaW5lcnxlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     name: "Whey Protein Isolate",
     price: 249.99,
     image:
-      "https://tse1.mm.bing.net/th/id/OIP.ELEgMYrxV907MSWfVQY-ngHaHa?pid=ImgDet&w=184&h=184&c=7&dpr=1.3&o=7&rm=3",
+      "https://images.unsplash.com/photo-1704650311190-7eeb9c4f6e11?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHdoZXklMjBwcm90ZWlufGVufDB8fDB8fHww",
   },
   {
     name: "Creatine Monohydrate",
     price: 899.99,
     image:
-      "https://images.unsplash.com/photo-1516035069371-29a1b244cc32?w=800&auto=format&fit=crop&q=60",
+      "https://images.unsplash.com/photo-1683394305929-5e7c8d942127?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y3JlYXRpbmV8ZW58MHx8MHx8fDA%3D",
   },
   {
     name: "Magnesium Glycinate",
     price: 79.99,
     image:
-      "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=800&auto=format&fit=crop&q=60",
+      "https://images.unsplash.com/photo-1729708273852-b63222c8b35d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFnbmVzaXVtJTIwZ2x5Y2luYXRlfGVufDB8fDB8fHww",
   },
   {
     name: "B-complex",
     price: 89.99,
     image:
-      "https://images.unsplash.com/photo-1527814050087-3793815479db?w=800&auto=format&fit=crop&q=60",
-  },
-  {
-    name: "Vitamin C",
-    price: 159.99,
-    image:
-      "https://images.unsplash.com/photo-1589492477829-5e65395b66cc?w=800&auto=format&fit=crop&q=60",
-  },
-  {
-    name: "Probiotics",
-    price: 449.99,
-    image:
-      "https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?w=800&auto=format&fit=crop&q=60",
+      "https://images.unsplash.com/photo-1729701494012-8e9d5c5ed6b7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YiUyMGNvbXBsZXh8ZW58MHx8MHx8fDA%3D",
   },
 ];
 
